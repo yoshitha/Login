bbb518d0fec330e37186b4ec2c1b80b3b3c8dc37
Yoshitha <yoshitha.kin@gmail.com>
2017-12-19 12:44:46 +0530

    Signup with email activation, Login, Logout, Home added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_project',
+		'USER': 'root',
+		'PASSWORD': 'yoshitha',
+		'HOST': 'localhost',
+		'PORT': 8000
     }
 }
 
